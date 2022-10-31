43af39db304bc02520027f067aa6ddd9aafa9e09
GIRESHCHATURVEDI <95335457+GIRESHCHATURVEDI@users.noreply.github.com>
2022-10-31 14:59:14 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String [] args) {
+      RWOnly ans  = new RWOnly() ;
+      ans.setName(10) ;   // nake variable name public or peotected....
+      System.out.print(ans.getName()) ;
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int name ;
+    public void setName(int x){
+        this.name = x ;
+    }
+    public int getName(){
+        return name ;
+    }
 }
